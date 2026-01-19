b27e3d487f1f86afffb43b731833f782dfe814a7
“Samuel <samuelsweke@gmail.com>
2026-01-19 14:57:27 +0000

    setup supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Missing EXPO_PUBLIC_SUPABASE_URL environment variable");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("Missing EXPO_PUBLIC_SUPABASE_ANON_KEY environment variable");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+    persistSession: true,
+    storage: AsyncStorage,
+  },
+});
